1c1a47568e6874911f58ae9e0f958ebfb852f8cd
Elneris Dang <elneris.dang@gmail.com>
2019-03-29 09:30:16 +0100

    modif du fichier connect en gitignore et ajout des données sensible

--- src/connect.php
+++ src/connect.php
@@ -2,6 +2,6 @@
 
 // Constante a définir pour se connecter à la BDD ( fichier initialement commit mais mit en gitignore pour sécuriser les données )
 
-define("DSN", "");
-define("USER", "");
-define("PASS", "");
+define("DSN", "mysql:host=localhost;dbname=checkpoint1");
+define("USER", "root");
+define("PASS", "root");
